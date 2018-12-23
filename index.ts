a4325ca220c59b5e7bf8487478cbf6d08434b931
blue-jam <blue-jam@users.noreply.github.com>
2018-12-23 21:08:31 +0900

    Separate HTML utilities to module

--- index.ts
+++ index.ts
@@ -1,43 +1,7 @@
 import Calendar = GoogleAppsScript.Calendar.Calendar;
 import {Xml} from "xml";
+import {getElementsByClassName, getElementsByTagName} from "./util/htmlParser";
 import {calculateDurationFromString} from "./util/time";
-
-const getElementsByClassName = (element, classToFind: string) => {
-    const data = [];
-    const descendantList = element.getDescendants();
-    descendantList.push(element);
-    for (const descendant of descendantList) {
-        const elt = descendant.asElement();
-        if (elt != null) {
-            let classes = elt.getAttribute("class");
-            if (classes !== null) {
-                classes = classes.getValue();
-                if (classes === classToFind) {
-                    data.push(elt);
-                } else {
-                    classes = classes.split(" ");
-                    for (const j in classes) {
-                        if (classes[j] === classToFind) {
-                            data.push(elt);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return data;
-};
-
-const getElementsByTagName = (element, tagName: string) => {
-    const data = [];
-    const descendantList = element.getDescendants();
-    for (const descendant of descendantList) {
-        const elt = descendant.asElement();
-        if (elt !== null && elt.getName() === tagName) { data.push(elt); }
-    }
-    return data;
-};
 
 interface IContest {
     title: string;
