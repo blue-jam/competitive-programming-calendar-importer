6d86dfe5db3801cfb49cec514340533948be2d5f
blue-jam <blue-jam@users.noreply.github.com>
2018-12-23 21:16:01 +0900

    Refactor htmlParser

--- util/htmlParser.ts
+++ util/htmlParser.ts
@@ -1,31 +1,33 @@
-export function getElementsByClassName(element, classToFind: string) {
+import Element = GoogleAppsScript.XML_Service.Element;
+
+export function getElementsByClassName(element: Element, classToFind: string): Element[] {
     const data = [];
-    const descendantList = element.getDescendants();
+    const descendantList = element.getDescendants()
+        .map((content) => content.asElement());
     descendantList.push(element);
+
     for (const descendant of descendantList) {
-        const elt = descendant.asElement();
-        if (elt != null) {
-            let classes = elt.getAttribute("class");
-            if (classes !== null) {
-                classes = classes.getValue();
-                if (classes === classToFind) {
-                    data.push(elt);
-                } else {
-                    classes = classes.split(" ");
-                    for (const j in classes) {
-                        if (classes[j] === classToFind) {
-                            data.push(elt);
-                            break;
-                        }
-                    }
-                }
-            }
+        if (descendant == null) {
+            continue;
+        }
+
+        const classes = descendant.getAttribute("class");
+
+        if (classes === null) {
+            continue;
+        }
+
+        const classList = classes.getValue().split(" ");
+
+        if (classList.indexOf(classToFind) !== -1) {
+            data.push(descendant);
         }
     }
+
     return data;
 }
 
-export function getElementsByTagName(element, tagName: string) {
+export function getElementsByTagName(element: Element, tagName: string): Element[] {
     const data = [];
     const descendantList = element.getDescendants();
     for (const descendant of descendantList) {
@@ -34,5 +36,6 @@ export function getElementsByTagName(element, tagName: string) {
             data.push(elt);
         }
     }
+
     return data;
 }
